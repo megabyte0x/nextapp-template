08a2eeeeccbd534927ac22714d7786ff2731e202
Yash Garg <yashygkf@gmail.com>
2022-09-13 15:16:18 +0530

    edit: set the deafult seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 // pages/_app.js
 import { ChakraProvider } from '@chakra-ui/react'
+import {DefaultSeo} from 'next-seo'
 import { AppProps } from 'next/app'
+
+import defaultSEOConfig from '../../next-seo.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <DefaultSeo {...defaultSEOConfig} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
