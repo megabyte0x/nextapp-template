423c16b57f91423ae5c35dcccd8e2e05920eaaaa
Yash Garg <yashygkf@gmail.com>
2022-09-13 16:31:48 +0530

    add: color provider added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,24 @@
-// pages/_app.js
-import { ChakraProvider, } from '@chakra-ui/react'
-import {DefaultSeo} from 'next-seo'
-import { AppProps } from 'next/app'
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { DefaultSeo } from "next-seo";
+import { AppProps } from "next/app";
 
-import defaultSEOConfig from '../../next-seo.config';
-import customTheme from '../../styles/theme';
+import defaultSEOConfig from "../../next-seo.config";
+import customTheme from "../../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={customTheme}>
       <DefaultSeo {...defaultSEOConfig} />
-      <Component {...pageProps} />
+      <ColorModeProvider
+        options={{
+          initialColorMode: "dark",
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
